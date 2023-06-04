d8898dc8d59aefe6188e920e82e5778d4031173d
Nicholas Spencer Watt <23640103@sun.ac.za>
2023-06-04 12:18:31 +0200

    AST

--- lib/ASTManager.cpp
+++ lib/ASTManager.cpp
@@ -1,5 +1,9 @@
 #include "ASTManager.h"
 
+const char* arithmetic_operators = "+-*/=";
+const char* logical_operators = "&!|";
+const char* separators = "()[]{};";
+
 struct Token* Lexer::lex(std::string* command){
     uint32_t pointer = 0;
     size_t command_length = command -> length();
@@ -16,8 +20,32 @@ struct Token* Lexer::lex(std::string* command){
 }
 
 struct Token* Lexer::generateToken(std::string* command, uint32_t* pointer, struct Token* last_token){
-    struct Token* new_token = (struct Token*)ps_malloc(sizeof(struct Token));
-    last_token -> next_token = new_token;
+    char character = command -> at(*pointer);
 
-    
+    if(isspace(character)){ // If it is whitespace, go to the next character
+        *pointer++;
+        return last_token;
+    }
+
+    struct Token* new_token = (struct Token*)ps_malloc(sizeof(struct Token));
+    if(last_token != nullptr) last_token -> next_token = new_token; // Only store the new token address if its not the first token.
+
+    if(isalpha(character)){ // Check if it is an identifier/keyword
+
+    } else if (isdigit(character)){ // Check if it is a number
+
+    } else if (character == '\"') { // Check if string literal
+
+    } else { // Else it is a separator
+
+    }
+
+}
+
+bool Lexer::isin(char* character, const char* array){
+    for(int i = 0; i < sizeof(array), i++){
+        if(*character == *(array + i)) return true;
+    }
+
+    return false;
 }
--- lib/config.h
+++ lib/config.h
@@ -9,18 +9,6 @@ constexpr size_t BROTLI_DECODER_BUFFER_SIZE = 4096;
 constexpr const char * MQTT_STORAGE_NVS_PATH = "/Connectivity/mqtt_nvs";
 
 
-/* RTOS Config */
-constexpr uint32_t APP_TASK_STACK = 32768;
-constexpr uint32_t CONTROL_TASK_STACK = 32768;
-constexpr uint32_t AST_TASK_STACK = 32768;
-constexpr uint32_t COMMS_TASK_STACK = 32768;
-
-constexpr uint8_t APP_QUEUE_SIZE = 10;
-constexpr uint8_t CONTROL_QUEUE_SIZE = 10;
-constexpr uint8_t AST_QUEUE_SIZE = 10;
-constexpr uint8_t COMMS_QUEUE_SIZE = 10;
-
-
 class SDRException : public std::exception
 {
 public:
